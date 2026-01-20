a614e42e7bb4ea71002857a54d89973fb12e3085
Naeem Tipu <naeem.tipu@riskident.com>
2026-01-20 17:58:18 +0100

    changed absolute path

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "null_resource" "k0sctl_apply" {
   }
 
   provisioner "local-exec" {
-    command     = "k0sctl apply --config ${abspath(path.module)}/../../k0s/k0sctl-libvirt.yaml"
+    command     = "k0sctl apply --config ${abspath(path.module)}/k0sctl-libvirt.yaml"
     working_dir = abspath(path.module)
   }
 }
